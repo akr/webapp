0d50262b321a50864ac12600dffcad2565fbdf96
Tanaka Akira <akr@fsij.org>
2004-09-13 04:32:25 +0000

    If it is run as application, take an argument as docroot or servlet.

--- webapp/webrick-servlet.rb
+++ webapp/webrick-servlet.rb
@@ -5,20 +5,22 @@ class WebApp
     LoadedServlets = {}
     def WEBrickServletHandler.get_instance(config, name)
       unless LoadedServlets[name]
-        begin
-          Thread.current[:webrick_load_servlet] = true
-          load name, true
-          if Thread.current[:webrick_load_servlet].respond_to? :call
-            LoadedServlets[name] = Thread.current[:webrick_load_servlet]
-          end
-        ensure
-          Thread.current[:webrick_load_servlet] = nil
-        end
-        unless LoadedServlets[name]
+        LoadedServlets[name] = load_servlet(name)
+      end
+      LoadedServlets[name]
+    end
+
+    def WEBrickServletHandler.load_servlet(path)
+      begin
+        Thread.current[:webrick_load_servlet] = true
+        load path, true
+        unless Thread.current[:webrick_load_servlet].respond_to? :call
           raise "WEBrick servlet is not registered: #{path}"
         end
+        return WEBrick::HTTPServlet::ProcHandler.new(Thread.current[:webrick_load_servlet])
+      ensure
+        Thread.current[:webrick_load_servlet] = nil
       end
-      WEBrick::HTTPServlet::ProcHandler.new(LoadedServlets[name])
     end
   end
 end
@@ -26,7 +28,24 @@ WEBrick::HTTPServlet::FileHandler.add_handler('webrick',
   WebApp::WEBrickServletHandler)
 
 if $0 == __FILE__
-  httpd = WEBrick::HTTPServer.new(:DocumentRoot => Dir.getwd, :Port => 10080)
-  trap(:INT){ httpd.shutdown }
-  httpd.start
+  # usage: [-p port] [docroot|servlet]
+  require 'optparse'
+  port = 10080
+  ARGV.options {|q|
+    q.def_option('--help', 'show this message') {puts q; exit(0)}
+    q.def_option('--port=portnum', 'specify server port') {|num| port = num.to_i }
+    q.parse!
+  }
+  docroot = ARGV.shift || Dir.getwd
+  if File.directory? docroot
+    httpd = WEBrick::HTTPServer.new(:DocumentRoot => docroot, :Port => port)
+    trap(:INT){ httpd.shutdown }
+    httpd.start
+  else
+    servlet = WebApp::WEBrickServletHandler.load_servlet(docroot)
+    httpd = WEBrick::HTTPServer.new(:Port => port)
+    trap(:INT){ httpd.shutdown }
+    httpd.mount("/", servlet)
+    httpd.start
+  end
 end
